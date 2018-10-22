fadc803438adf2e6f61a8f7b8e1f330f3082e5f0
Andrew Core <andrew.core@computrols.com>
2018-10-22 16:21:18 -0500

    initialized Kanban board, and various staff

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import time
 class Table(object):
     orders = []
+    orderStatus = None
     waiter = None #need to make a waiter class to store here
 
     def __init__(self, id, seating):
         self.id = id
         self.seating = seating    
     
-    def add_order(self, MenuItem):
-        self.orders.append(MenuItem)
+    def add_order(self, MenuItem, orderID):
+        self.orders.append([MenuItem, orderID])
 
     def total_bill(self):
         pass #add up the price of all MenuItems in orders[]
 
     def clear_table(self):
         orders = []
+
+    def place_order(self, orderID):
+        pass #Make the proper call to the KanBan board
 
     
 class Menu(object):
@@ -31,6 +35,30 @@ class MenuItem(object):
         self.name = name
         self.price = price
         self.mealType = mealType #app, entree, dessert, drink, etc.
+
+class KanBan(object):
+    toDo = None
+    inProgress = None
+    finished = None
+
+    def add_order(self, Table, order):
+        pass
+    def start_item(self):
+        pass
+    def finish_item(self):
+        pass
+
+class Staff(object):
+    pass
+
+class Waiter(Staff):
+    pass
+
+class Chef(Staff):
+    pass
+
+class Manager(Staff):
+    pass
 
 
 #testmenu = Menu()
